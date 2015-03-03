eca37db5ea850bad2144805763b0eece38965b78
Ben Karl <bkarl@cyrusinnovation.com>
2015-03-03 15:03:27 -0500

    Add external submission route and template as well as route, template and styles for thank you page

---
+++ app/controllers/extsubmission.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  breadCrumb: 'New Applicant',
+  candidateName: null,
+  candidateEmail: null,
+  selectedLanguage: null,
+  selectedLevel: null,
+
+  isFormIncomplete: Ember.computed.or('isCandidateIncomplete', 'isSubmissionIncomplete'),
+
+  isCandidateIncomplete: function() {
+    return Ember.isEmpty(this.get('candidateName')) ||
+      Ember.isEmpty(this.get('candidateEmail'));
+  }.property('candidateName', 'candidateEmail'),
+
+  isSubmissionIncomplete: function() {
+    return Ember.isEmpty(this.get('submission.emailText')) ||
+      Ember.isEmpty(this.get('submission.zipfile'));
+  }.property('submission.emailText', 'submission.zipfile'),
+
+  findCandidate: function() {
+    return this.store.find('candidate', { email: this.get('candidateEmail') }).then(function(results) {
+      return results.get('firstObject');
+    });
+  },
+
+  createSubmission: function() {
+    var submission = this.get('submission');
+    submission.set('candidateName', this.get('candidateName'));
+    submission.set('candidateEmail', this.get('candidateEmail'));
+    submission.set('level', this.get('selectedLevel'));
+    submission.set('language', this.get('selectedLanguage'));
+    return submission.save();
+  },
+
+  actions: {
+    submit: function() {
+      var self = this;
+      self.createSubmission() .then(function() {
+        self.transitionToRoute('/thanks');
+      });
+    }
+  }
+});
