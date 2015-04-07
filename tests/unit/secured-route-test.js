c1182cd47f3839b84c68984b7eb1c5aaf51596ec
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-07 13:44:48 -0400

    our first tiny little unit test for secured route

---
+++ tests/unit/secured-route-test.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import CodeTestBotApp from 'code-test-bot-app/app';
+import SecuredRoute from 'code-test-bot-app/routes/secured';
+import startApp from '../helpers/start-app';
+
+module("Unit: routes/secured", {
+  setup: function() {
+    window.CodeTestBotApp = startApp();
+  },
+  teardown: function() {
+    Ember.run(window.CodeTestBotApp, 'destroy');
+  }
+});
+
+test("if not authenticated, store attempted transition", function() {
+  visit('/submissions/80');
+  var expected = "/submissions/80";
+
+  andThen(function(){
+    equal(window.CodeTestBotApp.get('dataStore').getItem('attemptedTransition'), expected);
+  });
+});
