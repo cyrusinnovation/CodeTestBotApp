5c344f3c6875bd16a826570e495851b7f214ed1d
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-08 14:04:35 -0400

    When submission is closed show all assessments to everyone.

--- app/controllers/assessments/index.js
+++ app/controllers/assessments/index.js
@@ -2,14 +2,21 @@ import Ember from 'ember';
 import UserAwareControllerMixin from 'code-test-bot-app/mixins/user-aware-controller';
 
 var AssessmentsIndexController = Ember.ArrayController.extend(UserAwareControllerMixin, {
+    needs: ['submission'],
     itemController: 'assessments/item',
     userHasPublishedAssessment: false,
 
-    showAssessments: Ember.computed.or('userHasPublishedAssessment', 'isRecruiter', 'isInactive'),
+    showAssessments: Ember.computed.or('userHasPublishedAssessment', 'isRecruiter', 'isParentSubmissionClosed'),
 
     updateUserHasAssessment: function() {
         this.set('userHasPublishedAssessment', this.get('content').findBy('assessor.id', this.get('user.id')) !== undefined);
     }.observes('content.[]'),
+
+    isParentSubmissionClosed: function() {
+      var parentSubmission = this.get('controllers.submission');
+      var isSubmissionActive = parentSubmission.get('active');
+      return !isSubmissionActive;
+    }.property()
 });
 
 export default AssessmentsIndexController;
