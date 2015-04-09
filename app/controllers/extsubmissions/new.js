0a9900ca192c716216d1dc85c0803fce5fee09a4
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-09 16:31:51 -0400

    Merging ExtsubmissionsNew and SubmissionsNew templates by extracting differences into controllers

--- app/controllers/extsubmissions/new.js
+++ app/controllers/extsubmissions/new.js
@@ -3,5 +3,13 @@ import SubmissionNewController from 'code-test-bot-app/controllers/submissions/n
 export default SubmissionNewController.extend({
   breadCrumb: 'New Applicant',
   postSubmitURL: '/thanks',
-  isExternal: true
+  isExternal: true,
+  submitter: 'Your',
+  levelPrompt: "Select level of application",
+  languageLabel: "Programming Language",
+  languagePrompt: "Select the language your submission uses",
+  sourceLabel: "How Did You Find Us?",
+  notesLabel: "Notes",
+  notesPlaceholder: "Use this space for any general comments related to your submission that aren't already included in the README.",
+  zipfileLabel: "Submission Zipfile"
 });
