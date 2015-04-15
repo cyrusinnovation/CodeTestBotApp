22f6567fbe5b8dbf12b309886f8824b06f373920
Jeff Jia <jjia@cyrusinnovation.com>
2015-04-15 14:26:20 -0400

    Remove unused references from external submission model

--- app/models/extsubmission.js
+++ app/models/extsubmission.js
@@ -1,6 +1,3 @@
-import Ember from 'ember';
-import DS from 'ember-data';
-import Score from 'code-test-bot-app/models/score';
 import Submission from 'code-test-bot-app/models/submission';
 
 export default Submission.extend({
